eb8a5f95f75781f9666484fbb0c678184022ed3d
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2022-02-25 13:02:33 +0100

    ghj

---
+++ src/Pages/Calculator.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <div>
+            <input></input>
+            <input></input>
+            <button></button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Calculator;
