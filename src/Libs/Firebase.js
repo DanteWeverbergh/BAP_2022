001670803bf1e92a11f8c5f05b5d02ed2bc74f22
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2022-08-01 18:02:40 +0200

    .env

--- src/Libs/Firebase.js
+++ src/Libs/Firebase.js
@@ -4,6 +4,15 @@ import 'firebase/compat/auth';
 import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 import { updateProfile } from 'firebase/auth';
 
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
 const firebase = Firebase.initializeApp(config);
 const storage = getStorage();
 const { FieldValue } = Firebase.firestore;
