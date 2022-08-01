64e93ef6aaf27525ae8eaff8b73f8ab523508c24
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2022-08-01 17:55:59 +0200

    remove firebase creds

--- src/Libs/Firebase.js
+++ src/Libs/Firebase.js
@@ -4,15 +4,6 @@ import 'firebase/compat/auth';
 import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
 import { updateProfile } from 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyBpbQWDbR-GdEeVOGcW8Ux6mnS6xfe-rAI',
-  authDomain: 'gains-dd329.firebaseapp.com',
-  projectId: 'gains-dd329',
-  storageBucket: 'gains-dd329.appspot.com',
-  messagingSenderId: '719404966875',
-  appId: '1:719404966875:web:46d98729e34e11176c7dfe',
-};
-
 const firebase = Firebase.initializeApp(config);
 const storage = getStorage();
 const { FieldValue } = Firebase.firestore;
